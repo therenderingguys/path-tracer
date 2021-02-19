c0882644c3398b461890854616bfe93d133651e6
Daniel Kane <dpk2029@gmail.com>
2021-02-19 01:29:08 -0500

    moved generate ray functionality out of camera amd added vectors to describe camera orientation

--- src/raycast/camera.h
+++ src/raycast/camera.h
@@ -7,12 +7,29 @@
 class Camera {
 private:
   glm::vec3 mOrigin, mDirection;
+  float mFOV;
+  glm::vec3 mUp;
+  glm::vec3 mRight;
+
+private:
+  void recalculateOrientation();
 
 public:
-  Camera(glm::vec3 &o, glm::vec3 &d);
-  Ray generate_ray(float x, float y);
+  Camera();
+  Camera(glm::vec3 &o, glm::vec3 &d, float fov);
+  
   const glm::vec3 &direction() const { return mDirection; }
+  void setDirection(glm::vec3 &dir) { this->mDirection = dir; }
+
   const glm::vec3 &origin() const { return mOrigin; }
+  void setOrigin(glm::vec3 &org) { this->mOrigin = org; }
+
+  float getFOV() const { return mFOV; }
+  void setFOV(float fovX) { this->mFOV = glm::radians(fovX); }
+
+  const glm::vec3 &getUp() const { return mUp; }
+  const glm::vec3 &getRight() const { return mRight; }
+  
 };
 
 #endif // __CAMERA_H__
