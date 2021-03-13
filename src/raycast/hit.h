d5dd6ba4d3f694c42e7fe50132e909b48f6bcc94
Farzon Lotfi <1802579+farzonl@users.noreply.github.com>
2021-03-13 05:19:59 -0500

    add clang format and license check (#28)

--- src/raycast/hit.h
+++ src/raycast/hit.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2021 F. Lotfi & D. Kane All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 
 #ifndef __HIT_H__
 #define __HIT_H__
--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 F. Lotfi & D. Kane All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include "triangle.h"
 
 Triangle::Triangle(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2, glm::vec3 color,
