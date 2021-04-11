de470d130c3b07d30e681d91203c1e208a7d478a
Daniel Kane <dpk2029@gmail.com>
2021-04-11 04:21:22 -0400

    fixed path tracer import typo

--- src/renderer/pathTracer.cpp
+++ src/renderer/pathTracer.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include "PathTracer.h"
+#include "pathTracer.h"
 
 glm::vec3 PathTracer::colorPixel(const unsigned int i, const unsigned int j,
                                Scene &scene) {
