6d0e0eee61ff4df4b86c60b9785d697bdf679861
Farzon Lotfi <farzonl@icloud.com>
2021-04-29 03:23:38 -0400

    add test cases.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -9,27 +9,6 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-glm::vec3 getColor(int index) {
-  glm::vec3 red(1, 0.701, 0.729);
-  glm::vec3 green(0.729, 1, 0.788);
-  glm::vec3 blue(0.729, 1, 1);
-  glm::vec3 purple(0.945, 0.835, 0.976);
-  glm::vec3 gray(0.31, 0.31, 0.31);
-  if (index % 2 == 0) {
-    return red;
-  }
-  if (index % 3 == 0) {
-    return green;
-  }
-  if (index % 5 == 0) {
-    return blue;
-  }
-  if (index % 7 == 0) {
-    return purple;
-  }
-  return gray;
-}
-
 int main(int argc, char *argv[]) {
   if( argc != 2) {
         std::cout << "usage ./exec <wavefront_obj_file>.obj" << std::endl;
@@ -45,14 +24,14 @@ int main(int argc, char *argv[]) {
   cam.setFOV(90.0f);
   Scene scene(cam);
 
-  //glm::vec3 gray(0.31, 0.31, 0.31);
+  glm::vec3 gray(0.31, 0.31, 0.31);
   for (int i = 0; i < model.nfaces(); i++) {
     std::vector<int> face = model.face(i);
     if (face.size() == 3) {
       glm::vec3 v1 = model.vert(face[0]);
       glm::vec3 v2 = model.vert(face[1]);
       glm::vec3 v3 = model.vert(face[2]);
-      scene.addTriangle(v1, v2, v3, getColor(i));
+      scene.addTriangle(v1, v2, v3, gray);
     }
   }
 
@@ -65,7 +44,6 @@ int main(int argc, char *argv[]) {
   PixelBuffer *pb = gWindow.getPixelBuffer();
   pathTracer.setPixelBuffer(pb);
   pathTracer.renderScene();
-  //gWindow.insertDrawCallback(pathTracer.getDrawCallBack());
 
   gWindow.run();
 
---
+++ test/fileParsers/fileParserTest.cpp
@@ -0,0 +1,116 @@
+// Copyright (c) 2021 F. Lotfi & D. Kane All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <catch2/catch.hpp>
+#include <glm/glm.hpp>
+#include <fstream>
+#include <algorithm>
+
+#include "fileParsers/imageExporter.h"
+#include "fileParsers/model.h"
+#include "raycast/camera.h"
+#include "scene/scene.h"
+#include "renderer/pathTracer.h"
+
+//Note: We really just want to do a file hash here, but there is no built in library.
+// TODO find a portable hashing library.
+bool compare_files(const std::string& filename1, const std::string& filename2)
+{
+    std::ifstream file1(filename1, std::ifstream::ate | std::ifstream::binary); //open file at the end
+    std::ifstream file2(filename2, std::ifstream::ate | std::ifstream::binary); //open file at the end
+    const std::ifstream::pos_type fileSize = file1.tellg();
+
+    if (fileSize != file2.tellg()) {
+        return false; //different file size
+    }
+
+    file1.seekg(0); //rewind
+    file2.seekg(0); //rewind
+
+    std::istreambuf_iterator<char> begin1(file1);
+    std::istreambuf_iterator<char> begin2(file2);
+
+    return std::equal(begin1,std::istreambuf_iterator<char>(),begin2); //Second argument is end-of-range iterator
+}
+
+glm::vec3 getColor(int index) {
+  glm::vec3 red(1, 0.701, 0.729);
+  glm::vec3 green(0.729, 1, 0.788);
+  glm::vec3 blue(0.729, 1, 1);
+  glm::vec3 purple(0.945, 0.835, 0.976);
+  glm::vec3 gray(0.31, 0.31, 0.31);
+  if (index % 2 == 0) {
+    return red;
+  }
+  if (index % 3 == 0) {
+    return green;
+  }
+  if (index % 5 == 0) {
+    return blue;
+  }
+  if (index % 7 == 0) {
+    return purple;
+  }
+  return gray;
+}
+
+void ReadObjToPixelBuffer(std::string filename, std::string outFileName="test.ppm") {
+  Model model(filename.c_str());
+
+  glm::vec3 camOrign({0, 0, 2});
+  glm::vec3 camDir({0, 0, -1});
+  Camera cam;
+  cam.setOrigin(camOrign);
+  cam.setDirection(camDir);
+  cam.setFOV(90.0f);
+  Scene scene(cam);
+
+  for (int i = 0; i < model.nfaces(); i++) {
+    std::vector<int> face = model.face(i);
+    if (face.size() == 3) {
+      glm::vec3 v1 = model.vert(face[0]);
+      glm::vec3 v2 = model.vert(face[1]);
+      glm::vec3 v3 = model.vert(face[2]);
+      scene.addTriangle(v1, v2, v3, getColor(i));
+    }
+  }
+
+  PathTracer pathTracer;
+  pathTracer.setScene(std::make_unique<Scene>(scene));
+  PixelBuffer pb(320, 240, ColorChannels::RGB);
+  pathTracer.setPixelBuffer(&pb);
+  pathTracer.renderScene();
+  ImageExport::exportAsPPM(outFileName, &pb);
+}
+
+TEST_CASE("square Obj file Parse test", "[fileParsers pixelBuffer]") {
+
+  ReadObjToPixelBuffer("objFiles/square.obj");
+  REQUIRE(compare_files("sln/square.ppm", "test.ppm"));
+}
+
+TEST_CASE("cube obj file parser test", "[fileParsers pixelBuffer]") {
+  ReadObjToPixelBuffer("objFiles/cube.obj");
+  REQUIRE(compare_files("sln/cube.ppm", "test.ppm"));
+}
+
+TEST_CASE("head obj file parser test", "[fileParsers pixelBuffer]") {
+  ReadObjToPixelBuffer("objFiles/african_head.obj");  
+  REQUIRE(compare_files("sln/head.ppm", "test.ppm"));
+}
+
+TEST_CASE("monkey obj file parser test", "[fileParsers pixelBuffer]") {
+   ReadObjToPixelBuffer("objFiles/monkey.obj"); 
+   REQUIRE(compare_files("sln/monkey.ppm", "test.ppm"));
+}
+
+TEST_CASE("diablo obj file parser test", "[fileParsers pixelBuffer]") {
+   ReadObjToPixelBuffer("objFiles/diablo3_pose.obj"); 
+   REQUIRE(compare_files("sln/diablo.ppm", "test.ppm"));
+}
+
+TEST_CASE("Pyramid obj file parser test", "[fileParsers pixelBuffer]") {
+   ReadObjToPixelBuffer("objFiles/pyramid.obj");  
+   REQUIRE(compare_files("sln/pyramid.ppm", "test.ppm"));
+}
