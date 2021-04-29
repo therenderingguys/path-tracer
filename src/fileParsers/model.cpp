77b9e203077e6249da9d28efc5c5f746eb6de282
Farzon Lotfi <farzonl@icloud.com>
2021-04-29 01:14:25 -0400

    add a means of reading obj files

---
+++ src/fileParsers/model.cpp
@@ -0,0 +1,76 @@
+#include "model.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void Model::singlSlashFaceFormat(std::istringstream &iss,
+                                  std::vector<int> &f) {
+  if (iss.str().find("/") == std::string::npos) {
+    return;
+  }
+  int itrash, idx;
+  char trash;
+  while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+    idx--; // in wavefront obj all indices start at 1, not zero
+    f.push_back(idx);
+  }
+}
+void Model::doubleSlashFaceFormat(std::istringstream &iss,
+                                  std::vector<int> &f) {
+  if (iss.str().find("//") == std::string::npos) {
+    return;
+  }
+  int itrash, idx;
+  char trash;
+  while (iss >> idx >> trash >> trash >> itrash) {
+    idx--; // in wavefront obj all indices start at 1, not zero
+    f.push_back(idx);
+  }
+}
+void Model::spaceDelimitedFormat(std::istringstream &iss, std::vector<int> &f) {
+  int idx;
+  while (iss >> idx) {
+    idx--; // in wavefront obj all indices start at 1, not zero
+    f.push_back(idx);
+  }
+}
+
+Model::Model(const char *filename) : mVerts(), mFaces() {
+  std::ifstream in;
+  in.open(filename, std::ifstream::in);
+  if (in.fail())
+    return;
+  std::string line;
+  while (!in.eof()) {
+    std::getline(in, line);
+    std::istringstream iss(line.c_str());
+    char trash;
+    if (!line.compare(0, 2, "v ")) {
+      iss >> trash;
+      glm::vec3 v;
+      for (int i = 0; i < 3; i++)
+        iss >> v[i];
+      mVerts.push_back(v);
+    } else if (!line.compare(0, 2, "f ")) {
+      std::vector<int> f;
+      iss >> trash;
+      doubleSlashFaceFormat(iss, f);
+      singlSlashFaceFormat(iss, f);
+      spaceDelimitedFormat(iss, f);
+     
+      mFaces.push_back(f);
+    }
+  }
+  std::cerr << "# v# " << mVerts.size() << " f# " << mFaces.size() << std::endl;
+}
+
+Model::~Model() {}
+
+int Model::nverts() { return (int)mVerts.size(); }
+
+int Model::nfaces() { return (int)mFaces.size(); }
+
+std::vector<int> Model::face(int idx) { return mFaces[idx]; }
+
+glm::vec3 Model::vert(int i) { return mVerts[i]; }
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,10 +5,38 @@
 #include "renderer/pathTracer.h"
 #include "scene/scene.h"
 #include "window/glfwWindow.h"
+#include "fileParsers/model.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+glm::vec3 getColor(int index) {
+  glm::vec3 red(1, 0.701, 0.729);
+  glm::vec3 green(0.729, 1, 0.788);
+  glm::vec3 blue(0.729, 1, 1);
+  glm::vec3 purple(0.945, 0.835, 0.976);
+  glm::vec3 gray(0.31, 0.31, 0.31);
+  if (index % 2 == 0) {
+    return red;
+  }
+  if (index % 3 == 0) {
+    return green;
+  }
+  if (index % 5 == 0) {
+    return blue;
+  }
+  if (index % 7 == 0) {
+    return purple;
+  }
+  return gray;
+}
+
 int main(int argc, char *argv[]) {
+  if( argc != 2) {
+        std::cout << "usage ./exec <wavefront_obj_file>.obj" << std::endl;
+        return 0;
+  }
+  Model model(argv[1]);
+
   glm::vec3 camOrign({0, 0, 2});
   glm::vec3 camDir({0, 0, -1});
   Camera cam;
@@ -17,27 +45,16 @@ int main(int argc, char *argv[]) {
   cam.setFOV(90.0f);
   Scene scene(cam);
 
-  glm::vec3 red(1, 0.701, 0.729);
-  glm::vec3 green(0.729, 1, 0.788);
-  glm::vec3 blue(0.729, 1, 1);
-  glm::vec3 purple(0.945, 0.835, 0.976);
-
-  float z = -1;
-  glm::vec3 ulp(-1, 1, z);
-  glm::vec3 urp(1, 1, z);
-  glm::vec3 brp(1, -1, z);
-  glm::vec3 blp(-1, -1, z);
-  glm::vec3 cp(0, 0, z);
-
-  scene.addTriangle(ulp, urp, cp, red);
-  scene.addTriangle(urp, brp, cp, green);
-  scene.addTriangle(brp, blp, cp, blue);
-  scene.addTriangle(blp, ulp, cp, purple);
-
-  glm::vec3 pCenterUp(0, 1, -0.5);
-  glm::vec3 pRightBottom(2, 0, -1.5);
-  glm::vec3 pLeftBottom(-2, 0, -1.5);
-  scene.addTriangle(pCenterUp, pRightBottom, pLeftBottom, green);
+  //glm::vec3 gray(0.31, 0.31, 0.31);
+  for (int i = 0; i < model.nfaces(); i++) {
+    std::vector<int> face = model.face(i);
+    if (face.size() == 3) {
+      glm::vec3 v1 = model.vert(face[0]);
+      glm::vec3 v2 = model.vert(face[1]);
+      glm::vec3 v3 = model.vert(face[2]);
+      scene.addTriangle(v1, v2, v3, getColor(i));
+    }
+  }
 
   GLWindow gWindow("Test");
   gWindow.init();
@@ -47,8 +64,8 @@ int main(int argc, char *argv[]) {
 
   PixelBuffer *pb = gWindow.getPixelBuffer();
   pathTracer.setPixelBuffer(pb);
-
-  gWindow.insertDrawCallback(pathTracer.getDrawCallBack());
+  pathTracer.renderScene();
+  //gWindow.insertDrawCallback(pathTracer.getDrawCallBack());
 
   gWindow.run();
 
